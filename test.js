e0dc382f1b2ecc406fd13afef2a34f86ffdbed51
Roberto Meijide <robertomorpheo@gmail.com>
2019-01-03 12:18:49 +0100

    Más pruebas de variables

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 let message;
-message = 'Hello!';
-
+message = 'Hello!'; //guarda el string
 alert(message); //muestra el contenido de la variable
+
+let usuario = 'Rob', 
+    edad = 32, 
+    mensaje = 'Hola mundo';
+
+const cumple = '27.06.1986';
